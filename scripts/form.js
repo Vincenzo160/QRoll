0aa8effdb408ff84e4e630062c01c0ae196cb85f
Vincenzo Deluca <delucaavincenzo@gmail.com>
2023-04-15 19:03:55 +0200

    v1.2.0
    Separated JS to separate files, cleaned some functions

---
+++ scripts/form.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+    
+// Firebase Config
+const firebaseConfig = {
+apiKey: "AIzaSyDYymYZFqhXG6U5weoSltXIwJfinvBAQsc",
+authDomain: "qroll-9eefa.firebaseapp.com",
+databaseURL: "https://qroll-9eefa-default-rtdb.europe-west1.firebasedatabase.app",
+projectId: "qroll-9eefa",
+storageBucket: "qroll-9eefa.appspot.com",
+messagingSenderId: "199837968072",
+appId: "1:199837968072:web:78e7dae9cf3d5a62e1a7fd"
+};
+    
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Form 
+const db = getDatabase(app);
+set(ref(db, 'prod/nav/' + new Date().getTime(),), {
+        useragent: navigator.userAgent,
+        epoch: new Date().getTime(),
+        date: new Date().toUTCString(),
+        page: "/r"
+    });
+console.log(db, 'prod/form/' + document.getElementById('code').value + '/')
+document.getElementById('QRForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    set(ref(db, 'prod/form/' + document.getElementById('code').value + '/' + new Date().getTime()), {
+        QRoll_id: document.getElementById('code').value,
+        location: document.getElementById('subject').value,
+        epoch: new Date().getTime(),
+        date: new Date().toUTCString(),
+        useragent: navigator.userAgent
+    });
+    document.getElementById('QRForm').reset();
+    location.href='/QRoll/r/thx'
+    
+});
---
+++ scripts/interactions.js
@@ -0,0 +1,32 @@
+// No Id mode
+function noid() {
+    document.getElementById('QRForm').reset();
+    var code = document.getElementById('code')
+    var txtplc = document.getElementById('idnone')
+    var noidmode = document.getElementById('noidmode')
+    code.value = "999";
+    code.disabled = true;
+    code.style.display = "none";
+    noidmode.style.display = "none";
+    txtplc.style.display = "block";
+    document.getElementById("yt-embed").style.display = "none";
+    document.getElementById("yt-embed").remove;
+    document.getElementById("stop").style.display = "block";
+    toggleimg()
+}
+
+// Reference Image Toggle 
+function toggleimg() {
+    var qrimg = document.getElementById("qrimg");
+    var offtxt = document.getElementById("switchoff");
+    var ontxt = document.getElementById("switchon");
+    if (qrimg.style.display === "none") {
+    ontxt.style.display = "none";
+    offtxt.style.display = "block";
+    qrimg.style.display = "block";
+    } else {
+    ontxt.style.display = "block";
+    offtxt.style.display = "none";
+    qrimg.style.display = "none";
+    }
+}
